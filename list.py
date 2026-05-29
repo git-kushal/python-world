5bf4f862f47b32bbfa4c2127606651cf904703b1
git-kushal <daskushal2630@gmail.com>
2026-05-29 10:03:21 +0530

    adding just

--- list.py
+++ list.py
@@ -3,10 +3,16 @@ print(car_types) #it print all the data storde in the list
 print(len(car_types)) #it print the length of the data the stored
 print(car_types[-4:])
 print(car_types[2])
-a= "AUTO"
-if a in car_types:
-    print("yes this is present")
-else:
-    car_types.append(a)
-    print("-----succesfuly added-----")
-    print(car_types)
+
+while True:
+    a = input("Enter a car name (or 'quit' to exit): ")
+    
+    if a == "QUIT":
+        print("Goodbye!")
+        break
+    elif a in car_types:
+        print("Yes, this is already present!")
+    else:
+        car_types.append(a)
+        print("----- Successfully Added -----")
+        print(car_types)
