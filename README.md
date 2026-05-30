9f927273c2f036d3418a01cbaf8b59470402bf8c
git-kushal <daskushal2630@gmail.com>
2026-05-30 12:45:30 +0530

    adding some details on readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python-world
+
 this repo only for practicing python and building foundations of projects.
+
+<h1>-----1: Project one----</h1>
+<p>this is a <u>shop billing counter system</u>,which include <b>Adding team, Billing, Adding extra discounnt and Exit</b></p>
--- shop_billling_counter.py
+++ shop_billling_counter.py
@@ -11,28 +11,33 @@ def bill():
     discount = 0.1 * total if total > 1000 else 0
     print("\n---- Bill ----")
     for item in cart:
-        print(f"{item['name']} x {item['qty']} = {item['qty'] * item['price']}")
-        print(f"Price of per item: {item["price"]}")
+        if "name" in item:
+            print(f"{item['name']} x {item['qty']} = {item['qty'] * item['price']}")
+            print(f"Price per item: {item['price']}")
     print(f"Total: {total}")
     print(f"Discount: {discount}")
-    print(f"Payable: {total - discount}")
-    
+    final = total - discount
+    cart.append({"final": final})
+    print(f"Payable: {final}")
 
-
+def extra_discount():
+    extra = float(input("Add extra discount: "))
+    ex = cart[-1]["final"] - extra
+    print("Extra discount: ", ex)
 
 def menu():
     while True:
-        print("\n1. Add Item\n2. Bill\n3. Exit")
+        print("\n1. Add Item\n2. Bill\n3. Extra\n4. Exit")
         ch = int(input("Choice: "))
         if ch == 1:
             add_item()
         elif ch == 2:
             bill()
-                 
         elif ch == 3:
+            extra_discount()
+        elif ch == 4:
             break
         else:
             print("Invalid choice. Please try again.")
 
 menu()
-
