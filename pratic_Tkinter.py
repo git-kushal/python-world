d9c78b8afb4cc40e53bdc6d9f337410a35ac64f0
git-kushal <daskushal2630@gmail.com>
2026-06-07 10:43:08 +0530

    pratic like button with it's atributs, bg,fg,activebackgroung,photoimage

--- pratic_Tkinter.py
+++ pratic_Tkinter.py
@@ -1,10 +1,26 @@
 from tkinter import *
 window = Tk()
+
+def click():
+    print("hello")
+
+
+
 window.title("kuhals")
 window.geometry("420x420")
 icon=PhotoImage(file="alive.png")
 window.iconphoto(True,icon)
+window.config(background="#EBB3FB")
 
+
+butto=Button(window,text="type")
+butto.config(command=click)
+butto.config(bg="#C13383")
+butto.config(fg="#CC56FF")
+butto.config(activebackground="#CC56FF")
+image=PhotoImage(file="alive.png")
+butto.config(image=image)
+butto.pack()
 
 
 
